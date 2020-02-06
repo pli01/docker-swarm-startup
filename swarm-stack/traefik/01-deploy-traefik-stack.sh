5ccfed6cb7c8fcc0069b287a7573c66cffac0427
pli01 <pli01@users.noreply.github.com>
2020-02-06 11:53:29 +0100

    Add traefik dashboard

--- swarm-stack/traefik/01-deploy-traefik-stack.sh
+++ swarm-stack/traefik/01-deploy-traefik-stack.sh
@@ -1,2 +1,3 @@
+docker network create --opt com.docker.network.driver.mtu=1450  --driver overlay traefik-network
 docker login privateregistry
 docker stack deploy --compose-file=traefik-stack.yml --with-registry-auth traefik
